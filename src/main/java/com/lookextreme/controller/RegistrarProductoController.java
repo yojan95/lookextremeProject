64712af2ef85427a68d58f6c2625d5ffe8dbf8c2
fabiancrc124 <fabiancrc124@gmail.com>
2020-10-04 23:30:44 -0500

    actualizacion del controller/RegistrarProductoController.java RegistroP.xhtml y SalidaP.xhtml

--- src/main/java/com/lookextreme/controller/RegistrarProductoController.java
+++ src/main/java/com/lookextreme/controller/RegistrarProductoController.java
@@ -3,16 +3,19 @@ package com.lookextreme.controller;
 
 import com.lookextreme.Dao.ProductosFacadeLocal;
 import com.lookextreme.Dao.NombreproductoFacadeLocal;
+import com.lookextreme.Dao.MarcaFacadeLocal;
 import com.lookextreme.model.Administrador;
 import com.lookextreme.model.Productos;
 import com.lookextreme.model.Categorias;
 import com.lookextreme.model.Marca;
 import com.lookextreme.model.Nombreproducto;     
 import com.lookextreme.Dao.CategoriasFacadeLocal;
+import com.lookextreme.model.Usuario;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -26,6 +29,8 @@ public class RegistrarProductoController implements Serializable {
     private Categorias categorias;
     private Marca marcas;
     private Nombreproducto nombrep;
+    private Usuario usuario ;
+    
     
     @EJB 
     private CategoriasFacadeLocal categoriasEJB;
@@ -35,14 +40,32 @@ public class RegistrarProductoController implements Serializable {
     private NombreproductoFacadeLocal nombreEJB;
     private List<Nombreproducto> nombrepList;
     
+    @EJB
+    private MarcaFacadeLocal marcaEJB;
+    private List<Marca> marcaList;
+    
+    
     @PostConstruct 
     
     public void init(){
         productos = new Productos();
         administrador = new Administrador();
+        marcas = new Marca();
+        categorias = new Categorias();
+        nombrep = new Nombreproducto();
+        usuario = new Usuario();
         categoriasList = categoriasEJB.findAll();
         nombrepList = nombreEJB.findAll();
+        marcaList = marcaEJB.findAll();
                 
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     public Productos getProductos() {
@@ -77,6 +100,14 @@ public class RegistrarProductoController implements Serializable {
         this.nombrep = nombrep;
     }
 
+    public List<Marca> getMarcaList() {
+        return marcaList;
+    }
+
+    public void setMarcaList(List<Marca> marcaList) {
+        this.marcaList = marcaList;
+    }
+
   
 
 
@@ -101,8 +132,10 @@ public class RegistrarProductoController implements Serializable {
         
         try {
             productos.setCategoriasIdcategorias(categorias);
-            productos.setNombreProductoidNombreProducto(nombrep);
-            administrador.setUsuarioidUsuario(1);
+            productos.setNombreProductoidNombreProducto(nombrep);            
+            productos.setMarcaIdmarca(marcas);            
+            usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+            administrador.setUsuarioidUsuario(usuario.getIdUsuario());
             productos.setAdministradorusuarioidUsuario(administrador);
             productos.setEstado("registrado");
             productoEJB.create(productos);
